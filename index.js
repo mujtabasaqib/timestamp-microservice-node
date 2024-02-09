1e4c17c3fe48f9837d0cd80c4b6ff35cb54c3123
mujtabasaqib <138833032+mujtabasaqib@users.noreply.github.com>
2024-02-09 22:50:57 +0000

    Source files for project

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
-// index.js
-// where your node app starts
-
 // init project
 var express = require('express');
 var app = express();
 
-// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
+// enable CORS 
 var cors = require('cors');
 app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
@@ -14,19 +10,57 @@ app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 20
 app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
+app.get("/", (req, res) => {
   res.sendFile(__dirname + '/views/index.html');
 });
 
 
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
+// first API endpoint
+app.get("/api/hello", (req, res) => {
   res.json({greeting: 'hello API'});
 });
 
+// date time for now
+app.get("/api", (req, res) => {
+  let date = new Date();
+  res.json({unix: date.getTime(), utc: date.toUTCString()});
+});
 
+// date reqs
+app.get('/api/:date', (req, res) => {
+  let date = req.params.date; //input date
 
-// listen for requests :)
+  const invalidDate = (unix,utc) => {
+    if(unix===null || utc ==="Invalid Date"){
+       return true;
+    }
+  }
+
+  // if the passed string has a dash it is a gmt date
+  if(date.includes("-")){
+    let unix = new Date(date).getTime();
+    let utc = new Date(date).toUTCString();
+    if(invalidDate(unix,utc)){
+      res.json({ error : "Invalid Date" });
+    }
+    else{
+      res.json({unix: unix, utc: utc});
+    }
+  }
+  // if the passed string is a unix timestamp
+  else {
+    let unix = parseInt(date);
+    let utc = new Date(unix).toUTCString();
+    if(invalidDate(unix,utc)){
+      res.json({ error : "Invalid Date" });
+    }
+    else{
+      res.json({unix: unix, utc: utc});
+    }
+  }
+});
+
+// listen for requests
 var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+  console.log('App is listening on port ' + listener.address().port);
 });
